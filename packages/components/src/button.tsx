5d7319e88376eecb0a8e63ba4fa869c1ccc0583c
Turbobot <turbobot@vercel.com>
2023-11-01 01:03:32 +0900

    feat(create-turbo): apply official-starter transform

--- packages/components/src/button.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./button.module.css";
-
-export const Button = () => (
-  <button type="button" data-my-button className={styles.button}>
-    Button
-  </button>
-);
--- packages/components/src/input.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import styles from "./input.module.css";
-
-export const Input: FC = () => {
-  return <input type="text" className={styles.input} />;
-};
